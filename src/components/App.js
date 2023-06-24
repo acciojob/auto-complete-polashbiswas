4f15e4f64220f6ee26bf2c3b5eb41f8419c54514
Polash Biswas <polashbiswasit19@gmail.com>
2023-06-24 15:34:20 +0530

    submitting code-Sat Jun 24 2023 15:34:16 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,34 @@
 
-import React from "react";
+
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
 
+const fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig"];
+
+
 const App = () => {
+  const [search, setSearch] = useState("");
+  const [filteredItem, setFilteredItem] = useState([...fruits]);
+
+  useEffect(() => {
+    let filteredFruits = fruits.filter((fruit) => fruit.includes(search));
+    setFilteredItem(filteredFruits);
+  }, [search])
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>Search item</h1>
+      <input type="text" onInput={(e) => setSearch(e.target.value)}></input>
+      <ul>
+        {
+          filteredItem.map((filteredFruit) => (
+            <li>{filteredFruit}</li>
+          ))
+        }
+      </ul>
+
     </div>
   )
 }
 
-export default App
+export default App;
